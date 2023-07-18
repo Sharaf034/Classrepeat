777ee61eae6b0acdb50f9f320e16e6be7bc3b012
Sharaf034 <sharafet@code.edu.az>
2023-07-18 23:29:47 +0400

    Add file

---
+++ Classrepeat/Classrepeat/Program.cs
@@ -0,0 +1,194 @@
+﻿
+
+
+
+
+
+//string name = "Fidan";
+////Console.WriteLine(name.Contains("i"));
+////Console.WriteLine(name.Equals("Fidan"));
+////Console.WriteLine(name.EndsWith("n"));
+////var result = name.Substring(1, 2);
+////Console.WriteLine(name.LastIndexOf("n"));
+////Console.WriteLine(name);
+////Console.WriteLine(name.GetType());
+
+
+//static bool Search(string str , string searchText)
+//{
+//    return str.Trim().ToLower().Contains(searchText.Trim().ToLower());
+//}
+
+
+//if (Search("salam", "can"))
+//{
+//    Console.WriteLine("Found");
+//}
+//else
+//{
+//    Console.WriteLine("Notfound");
+//}
+
+
+
+
+//int[] arr1 = new int[2];
+//int[] arr2 = new int[2] { 1, 5 };
+
+//int[] numbers = { 1, 2, 3, 4, 5 };
+
+//Console.WriteLine(numbers.Any(m=>m==55));
+
+//Array.Reverse(numbers);
+
+//foreach (var item in args)
+//{
+//    Console.WriteLine(item);
+//}
+
+//Console.WriteLine(numbers.FirstOrDefault());
+
+
+
+
+//int[] numbers = { 1, 2, 3, 4, 5 };
+
+//Array.Sort(numbers);
+
+////var data = numbers.OrderByDescending(x => x).ToArray();
+
+////foreach (var item in data)
+////{
+////    Console.WriteLine(item);
+////}
+
+
+
+//int[] nums = new int[3];
+//nums[0] = 20;
+//nums[1] = 30;
+//nums[2] = 22;
+
+
+//foreach (var item in args)
+//{
+//    Console.WriteLine(item);
+//}
+
+//Console.WriteLine(nums.Count());
+
+
+
+
+//using System.Security.AccessControl;
+//using System.Xml.Linq;
+
+//var stu1 = new
+//{
+//    name = "Ismayil",
+//    surname = "Efendizade",
+//    age = "29"
+//};
+
+
+
+//var stu2 = new
+//{
+//    name = "Xeyyam",
+//    surname = "Isgenderov",
+//    age = "29"
+//};
+
+
+
+//var stu3 = new
+//{
+//    name = "Cahangir",
+//    surname = "Axundov",
+//    age = "20"
+//};
+
+
+////string stu1 = $"{name} {surname} {age}";
+////string stu2 = $"{name} {surname} {age}";
+////string stu3 = $"{name} {surname} {age}";
+
+
+////Console.WriteLine(stu1);
+////Console.WriteLine(stu2);
+////Console.WriteLine(stu3);
+
+
+//Object[] student = {stu1 , stu2 ,stu3 };
+//Console.WriteLine(stu1.name + "  " + stu1.surname);
+////{
+////    foreach (var item in student)
+////    {
+////        Console.WriteLine(item);
+////    }
+////}   
+
+
+
+//Student stu1 = new()
+//{
+//    name = "Ismayil",
+//    surname = "Efendizade",
+//    age = 29,
+//    adress = "Yasamal"
+//};
+
+
+
+//Student stu2 = new()
+//{
+//    name = "Xeyyam",
+//    surname = "Isgenderov",
+//    age = 29,
+//    adress = "Yasamal"
+//};
+
+
+//Student stu3 = new()
+//{
+//    name = "Kubra",
+//    surname = "Memmedova",
+//    age = 25,
+//    adress = "Xalqlar"
+//};
+
+//Console.WriteLine(stu1.name + "  " + stu1.surname + "  " + stu1.age);
+
+
+//Student[] students = { stu1, stu2, stu3 };
+//{
+//    foreach (var item in students)
+//    {
+//        Console.WriteLine(item.name + "  " + item.surname + "  " + item.age);
+//    }
+//}
+
+
+
+//class Student
+//{
+//    public string name;
+//    public string surname;
+//    public int age;
+//    public string adress;
+//}
+
+
+//Book book1 = new()
+//{
+//    name = "Xosrov ve Shirin",
+//    autor = "Nizami"
+//};
+
+//Console.WriteLine(book1.name + "  " + book1.autor);
+
+
+
+
+
+
